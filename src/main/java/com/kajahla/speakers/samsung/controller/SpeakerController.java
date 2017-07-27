1d538e25dc7803983c21c48038d84d8b439fc2ec
Kamil Saykali <saykalik@hotmail.com>
2017-07-27 23:02:12 +0200

    - Added Service Discovery to get IP and name of Speaker (so its not hard coded) - TODO: Call each speaker to get MAC address - TODO: Group call to master is failing

--- src/main/java/com/kajahla/speakers/samsung/controller/SpeakerController.java
+++ src/main/java/com/kajahla/speakers/samsung/controller/SpeakerController.java
@@ -8,13 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceListener;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.URL;
-import java.util.ArrayList;
+import java.net.UnknownHostException;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Iterator;
 
 import static org.springframework.http.HttpHeaders.USER_AGENT;
 
@@ -23,9 +28,28 @@ public class SpeakerController {
 
     private HashMap<String, SpeakerInfo> speakers = new HashMap<>();
     private HashMap<String, GroupInfo> speakerGroups = new HashMap<>();
-
+    private JmDNS jmdns = null;
 
     public SpeakerController() {
+        try {
+            // Create a JmDNS instance
+            jmdns = JmDNS.create(InetAddress.getLocalHost());
+
+            // Add a service listener
+            jmdns.addServiceListener("_spotify-connect._tcp.local.", new SampleListener());
+
+            // Wait a bit
+            Thread.sleep(30000);
+
+        } catch (UnknownHostException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
+
         SpeakerInfo kitchen = new SpeakerInfo("kitchen", "192.168.10.197", "55001","f8:77:b8:c4:8f:a1");
         SpeakerInfo livingroom = new SpeakerInfo("livingroom", "192.168.10.238", "55001","f8:77:b8:c4:8f:ca");
         SpeakerInfo boysroom = new SpeakerInfo("boysroom", "192.168.10.200", "55001","f8:77:b8:c5:55:a0");
@@ -37,6 +61,37 @@ public class SpeakerController {
         speakers.put(bathroom.getName(), bathroom);
     }
 
+    private class SampleListener implements ServiceListener {
+        @Override
+        public void serviceAdded(ServiceEvent event) {
+//            System.out.println("Service added: " + event.getInfo());
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            jmdns.requestServiceInfo(event.getType(), event.getName(), 1);
+        }
+
+        @Override
+        public void serviceRemoved(ServiceEvent event) {
+//            System.out.println("Service removed: " + event.getInfo());
+        }
+
+        @Override
+        public void serviceResolved(ServiceEvent event) {
+//            System.out.println("Service resolved: " + event.getInfo());
+
+            if (event.getInfo().getInetAddresses() != null && event.getInfo().getInetAddresses().length > 0) {
+                System.out.println("Service name: " + event.getName());
+                System.out.println("Service ip: " + event.getInfo().getInetAddresses()[0].getHostAddress());
+            }
+        }
+    }
+
+
     @RequestMapping("/group")
     public ResponseEntity Group(@RequestParam(value="group_name", required = false) String groupName,
                                 @RequestParam(value="speaker") String[] speakerName) {
@@ -47,7 +102,7 @@ public class SpeakerController {
 
         // Check to make sure the group doesn't already exist
         GroupInfo groupInfo = null;
-        if (groupName != null && groupName.length() > 0) {
+        if (groupName.length() > 0) {
             groupInfo = speakerGroups.get(groupName);
             if (groupInfo != null)
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Group already exists");
@@ -59,7 +114,7 @@ public class SpeakerController {
 
         // Create a group and send the command to the speakers to join
         groupInfo = new GroupInfo();
-        if (groupName != null && groupName.length() > 0)
+        if (groupName.length() > 0)
             groupInfo.setName(groupName);
         else
             groupInfo.generateName();
@@ -78,45 +133,15 @@ public class SpeakerController {
                 "<p type=\"cdata\" name=\"audiosourcename\" val=\"empty\"><![CDATA[" + masterSpeaker.getName() + "]]></p>" +
                 "<p type=\"str\" name=\"audiosourcetype\" val=\"speaker\"/>";
 
-        for (Map.Entry<String, SpeakerInfo> entry : groupInfo.getSpeakerMap().entrySet()) {
-            String key = entry.getKey();
-            SpeakerInfo slaveSpeaker = entry.getValue();
-
-            if (slaveSpeaker.getName() == masterSpeaker.getName())
-                continue;
-
+        Iterator it = groupInfo.getSpeakerMap().entrySet().iterator();
+        while (it.hasNext()) {
+            SpeakerInfo slaveSpeaker = (SpeakerInfo)it.next();
             masterUrl += "<p type=\"str\" name=\"subspkip\" val=\"" + slaveSpeaker.getIp() + "\"/>" +
                     "<p type=\"str\" name=\"subspkmacaddr\" val=\"" + slaveSpeaker.getMac() + "\"/>";
         }
 
-        // Generate all slave urls
-        ArrayList<String> slaveUrls = new ArrayList<>();
-        for (Map.Entry<String, SpeakerInfo> entry : groupInfo.getSpeakerMap().entrySet()) {
-            String key = entry.getKey();
-            SpeakerInfo slaveSpeaker = entry.getValue();
-
-            if (slaveSpeaker.getName() == masterSpeaker.getName())
-                continue;
-
-            String slaveUrl = "http://" + slaveSpeaker.getIp() + ":" + slaveSpeaker.getPort() +
-                    "/UIC?cmd=<pwron>on</pwron><name>SetMultispkGroup</name>" +
-                    "<p type=\"cdata\" name=\"name\" val=\"empty\"><![CDATA[" + groupInfo.getName() + "]]></p>" +
-                    "<p type=\"dec\" name=\"index\" val=\"1\"/>" +
-                    "<p type=\"str\" name=\"type\" val=\"sub\"/>" +
-                    "<p type=\"dec\" name=\"spknum\" val=\"" + groupInfo.getNumSpeakers() + "\"/>" +
-                    "<p type=\"str\" name=\"mainspkip\" val=\"" + masterSpeaker.getIp() + "\"/>" +
-                    "<p type=\"str\" name=\"mainspkmacaddr\" val=\"" + masterSpeaker.getMac() + "\"/>";
-
-            slaveUrls.add(slaveUrl);
-        }
-
-
         try {
-
             sendGet(masterUrl);
-            for (String slaveUrl : slaveUrls)
-                sendGet(slaveUrl);
-
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -130,7 +155,7 @@ public class SpeakerController {
 
         // Check to make sure the group exists.
         GroupInfo groupInfo = null;
-        if (groupName != null && groupName.length() > 0) {
+        if (groupName.length() > 0) {
             groupInfo = speakerGroups.get(groupName);
             if (groupInfo == null)
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
@@ -142,20 +167,6 @@ public class SpeakerController {
                 sendGet(url);
             } catch (Exception ex) {
                 ex.printStackTrace();
-            }
-        }
-        // There are no groups so go through all speakers and ungroup them
-        else {
-
-            for (Map.Entry<String, SpeakerInfo> entry : speakers.entrySet()) {
-                SpeakerInfo speaker = entry.getValue();
-
-                String url = "http://" + speaker.getIp() + ":" + speaker.getPort() + "/UIC?cmd=<name>SetUngroup</name>";
-                try {
-                    sendGet(url);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
             }
         }
 
@@ -174,10 +185,8 @@ public class SpeakerController {
         //add request header
         con.setRequestProperty("User-Agent", USER_AGENT);
 
-        System.out.println("\nSending 'GET' request to URL : " + url);
-
         int responseCode = con.getResponseCode();
-
+        System.out.println("\nSending 'GET' request to URL : " + url);
         System.out.println("Response Code : " + responseCode);
 
         BufferedReader in = new BufferedReader(
